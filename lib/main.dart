01c3598a46dd94c666ac09a61039a0c601481db0
McHead <michalchajdus@gmail.com>
2023-08-05 20:19:02 +0200

    auto updater

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:path_provider/path_provider.dart';
 import 'home_page.dart';
+import 'package:http/http.dart' as http;
+import 'package:open_file/open_file.dart';
 
 Future<void> main() async {
   await dotenv.load();
@@ -26,8 +31,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  void initState() {
+    super.initState();
+    checkAndUpdate();
+  }
+
+  Future<void> checkAndUpdate() async {
+    bool hasUpdates = await checkForUpdates();
+    if (hasUpdates) {
+      // ignore: use_build_context_synchronously
+      showUpdateAlertDialog(context); // Show the update alert dialog
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,30 +124,39 @@ class LoginPage extends StatelessWidget {
               ),
               child: const Text('Continue without logging in'),
             ),
-            Padding(
-              padding: const EdgeInsets.all(48.0),
-              child: ElevatedButton(
-                onPressed: () {
-                 /* readVersionFile().then((version) {
-                    print(version); // The actual version will be printed here
-                    // You can use the version in any way you need.
-                  }).catchError((error) {
-                    print('Error reading version: $error');
-                  });*/
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  foregroundColor: Colors.black,
-                  minimumSize: const Size(200, 40),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5)),
-                ),
-                child: const Text('Check for updates'),
-              ),
-            ),
           ],
         ),
       ),
+    );
+  }
+
+  Future<void> showUpdateAlertDialog(BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('New Update Available'),
+          content: const Text(
+              'There is a new version of the app available. Do you want to update now?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog without updating
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Implement the update process here, such as redirecting to app store
+                // or performing an in-app update using a package like `flutter_inappupdate`.
+                // For the sake of this example, we will just close the dialog.
+                updateApp();
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -137,13 +171,57 @@ Future<String> readVersionFile() async {
   return contents;
 }
 
-/*Future<String> getGithubVersion() async {
-  String? githubVersionURL = dotenv.env['API_URL'];
+Future<String> getGithubVersion() async {
+  String? githubVersionURL = dotenv.env['GITHUB_VERSION_URL'];
+  String version = '';
+
   try {
-    contents = await rootBundle.loadString('.version');
-  } catch (error) {
-    contents = ''; // Handle the case when the version file cannot be read
+    final response = await http.get(Uri.parse('$githubVersionURL'));
+
+    if (response.statusCode == 200) {
+      version = response.body.trim();
+    } else {
+      throw Exception('Failed to fetch GitHub version');
+    }
+  } catch (e) {
+    // Handle any network-related errors
+    print('Error fetching GitHub version: $e');
+    throw Exception('Failed to fetch GitHub version');
   }
-  return contents;
+
+  return version;
 }
-*/
+
+Future<bool> checkForUpdates() async {
+  String localVersion = await readVersionFile();
+  String githubVersion = await getGithubVersion();
+
+  if (localVersion.isNotEmpty && localVersion != githubVersion) {
+    print('There is new version avaliable');
+    return true;
+  } else {
+    print('Current version is up to date');
+    return false;
+  }
+}
+
+Future<void> updateApp() async {
+  // Replace "your_update_url" with the URL where the updated APK is hosted.
+  String? githubApkURL = dotenv.env['GITHUB_APK_URL'];
+
+  // Download the updated APK to a temporary directory
+  final tempDir = await getTemporaryDirectory();
+  final updatedApkPath = "${tempDir.path}/app-debug.apk";
+
+  final response = await http.get(Uri.parse(githubApkURL!));
+  if (response.statusCode == 200) {
+    
+    final file = File(updatedApkPath);
+    await file.writeAsBytes(response.bodyBytes);
+    print('Downloaded newer version');
+
+    // Install the updated APK
+    final success = await OpenFile.open(updatedApkPath);
+  }
+
+}

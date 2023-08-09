e55174b9a3fe29f1b63fddff3c3118a13badcc2b
McHead <michalchajdus@gmail.com>
2023-08-09 17:58:56 +0200

    changed background color back

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.brown,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
